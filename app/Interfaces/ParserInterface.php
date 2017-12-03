45a2fd16dc16ca97dd5ad088d5ecec7788a61e70
Carlos Eduardo L. Braz <carloseduardolbraz@gmail.com>
2017-12-03 11:51:17 -0200

    Correção ParseInterface

--- app/Interfaces/ParserInterface.php
+++ app/Interfaces/ParserInterface.php
@@ -3,6 +3,6 @@ namespace Interfaces;
 
 interface ParserInterface
 {
-    public static function encode($objectToEncode) : ParserInterface;
-    public static function decode(ParserInterface $parserInterface);
+    public static function encode($objectToEncode) : Interger;
+    public static function decode(ParserInterface $parserInterface) : ParseInterface;
 }
