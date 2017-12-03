3f9652b3449b513f323954d33589ec9f0801bb74
Carlos Eduardo L. Braz <carloseduardolbraz@gmail.com>
2017-12-03 12:16:13 -0200

    Subindo Alteração do ParseInterface

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -32,7 +32,8 @@ class UserController
     */
     public function login(Request $request)
     {        
-        $user = new User(['login' => 'prometheus', 'password' => 'prometheus'])->save();
+        $user = new User(['login' => 'prometheus', 'password' => 'prometheus']);
+        $user->save();
         dd($user);
         $user = $this->userRepositoryEloquent->findWhere(
             [
