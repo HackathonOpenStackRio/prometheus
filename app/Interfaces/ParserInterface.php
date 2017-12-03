82bb1f61558426203d4146817ffed3284346836c
Carlos Eduardo L. Braz <carloseduardolbraz@gmail.com>
2017-12-03 11:52:52 -0200

    Subindo Alteração do ParseInterface

--- app/Interfaces/ParserInterface.php
+++ app/Interfaces/ParserInterface.php
@@ -3,6 +3,6 @@ namespace Interfaces;
 
 interface ParserInterface
 {
-    public static function encode($objectToEncode) : Interger;
+    public static function encode($objectToEncode) : String;
     public static function decode(ParserInterface $parserInterface) : ParseInterface;
 }
